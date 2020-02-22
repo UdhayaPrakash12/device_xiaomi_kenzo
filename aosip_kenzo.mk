68113a31b3d7220d80768e9baf1f79e7c7be9f84
UdhayaPrakash12 <35563826+UdhayaPrakash12@users.noreply.github.com>
2020-02-22 13:00:11 +0530

    Update and rename aosp_kenzo.mk to aosip_kenzo.mk

--- aosip_kenzo.mk
+++ aosip_kenzo.mk
@@ -16,15 +16,17 @@
 $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 
 
-# Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/common.mk)
-
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOTANIMATION_RES := 1080
+IS_PHONE := true
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := aosp_kenzo
+PRODUCT_NAME := aosip_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
